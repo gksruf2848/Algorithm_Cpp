3b37c7c6e4d74f53406a2ede2a49025e0e9cc8e5
gksruf2848 <4455qqq@naver.com>
2022-12-01 18:32:38 +0900

    .

---
+++ 2442.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int j = 1; j <= n; j++) {
+		for (int i = 0; i < n-j; i++) cout << " ";
+		for (int i = 0; i < 2*j-1; i++) cout << "*";
+		cout << "\n";
+	}
+}
